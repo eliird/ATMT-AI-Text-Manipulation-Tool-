1248f72ed27e61f49a5bc16177842a917e1f1f7a
irdali <irdali.durrani@fixstars.com>
2026-01-02 15:31:42 +0900

    hotkey press logic

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,21 @@ use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::window::WindowId;
 
+use tray_icon::{
+    menu::{Menu, MenuItem, MenuEvent, MenuId},
+    TrayIcon, TrayIconBuilder,
+};
 
-struct App;
+use global_hotkey::{
+    hotkey::{Code, HotKey, Modifiers},
+    GlobalHotKeyEvent, GlobalHotKeyManager,
+};
+
+struct App{
+    tray_icon: Option<TrayIcon>,
+    quit_id: MenuId,
+    hotkey_manager: GlobalHotKeyManager,
+}
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, _event_loop: &ActiveEventLoop) {
@@ -17,18 +30,56 @@ impl ApplicationHandler for App {
             _id: WindowId,
             _event: WindowEvent,
         ) {
-        // we do nothing for now will update later
+        // No window events to handle
+    }
+
+    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+        // check for menu events
+        if let Ok(event) = MenuEvent::receiver().try_recv(){
+            if event.id == self.quit_id {
+                println!("Quit menu item clicked. Exiting...");
+                event_loop.exit();
+            }
+        };
+
+        // check for the hotkey events
+        if let Ok(event) = GlobalHotKeyEvent::receiver().try_recv() {
+            println!("Hotkey event received: {:?}", event);
+        }
     }
 }
 fn main() {
 
     let event_loop = EventLoop::new().unwrap();
-
     // keep running until explicitly told to stop
     event_loop.set_control_flow(ControlFlow::Wait);
 
-    println!("Event loop started. Press Ctrl+C to exit.");
+    //create a tray menu
+    let menu = Menu::new();
+    let quite_item = MenuItem::new("Quit", true, None);
+    menu.append(&quite_item).unwrap();
+    let quit_id = quite_item.id().clone();
 
-    let mut app = App;
+    // create tray icon, empty for now
+    let tray_icon = TrayIconBuilder::new()
+        .with_menu(Box::new(menu))
+        .build()
+        .unwrap();
+    
+    // Register a global hotkey Ctrl+Shift+H
+    let hotkey_manager = GlobalHotKeyManager::new().unwrap();
+    let hotkey = HotKey::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyH);
+    hotkey_manager.register(hotkey).unwrap();
+
+    println!("Tray icon created. right click to see the menu.");
+    
+
+    let mut app = App { 
+        tray_icon: Some(tray_icon),
+        quit_id: quit_id,
+        hotkey_manager: hotkey_manager,
+    };
+       // listen for menu events
+    println!("Event loop started. Press Ctrl+C to exit.");
     event_loop.run_app(&mut app).unwrap();
 }
